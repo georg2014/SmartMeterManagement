208d002a5de1117a2a777c3df2e45582e6b5d2ea
SirFailaL0t <jakob-feldmann@outlook.com>
2017-06-20 16:54:25 +0200

    stylesheet css

--- workspace/SmartMeterManagementWeb/src/de/tub/as/smm/UserServlet.java
+++ workspace/SmartMeterManagementWeb/src/de/tub/as/smm/UserServlet.java
@@ -31,7 +31,7 @@ public class UserServlet extends HttpServlet {
         HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     	// Display the list of guests:
-        request.setAttribute("user1", userDao.getAllUsers());
+        request.setAttribute("user", userDao.getAllUsers());
         request.getRequestDispatcher("/user.jsp").forward(request, response);
  		
     }
